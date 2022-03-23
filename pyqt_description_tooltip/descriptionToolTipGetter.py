7416943f36e64e3fa9d314881eeca2e4cff456a1
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-24 07:31:04 +0900

    Doesn't include description part if there is no description

--- pyqt_description_tooltip/descriptionToolTipGetter.py
+++ pyqt_description_tooltip/descriptionToolTipGetter.py
@@ -5,6 +5,8 @@ class DescriptionToolTipGetter:
                    shortcut_color: str = '#AAA', description_color: str = '#AAA'):
         tooltip_str = f'''
         <p>{title} <span style=\'color: {shortcut_color};\'>{shortcut}</span></p>
-        <p style=\'color: {description_color};\'>{description}</p>
         '''
+        if description:
+            description_str = f'<p style=\'color: {description_color};\'>{description}</p>'
+            tooltip_str += description_str
         return tooltip_str
