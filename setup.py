36950b0d395344e0cd5fa2d75b3f69472a9deb88
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-14 07:14:06 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-description-tooltip',
@@ -9,4 +16,6 @@ setup(
     packages=find_packages(),
     description='PyQt QToolTip that user can add description and so on.',
     url='https://github.com/yjg30737/pyqt-description-tooltip.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description
 )
